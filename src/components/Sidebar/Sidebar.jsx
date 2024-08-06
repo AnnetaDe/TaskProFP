810532d4b13d56f7ff40f4aff511fc94d9f2a127
Yelyzaveta31 <Didenkoelizabeth@gmail.com>
2024-08-06 12:50:11 +0300

    add sidebar styles css

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,30 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 import CreateNewBoard from './CreateNewBoard/CreateNewBoard'
 import MyBoards from './MyBoards/MyBoards'
 import NeedHelp from './NeedHelp/NeedHelp'
 import LogOut from './LogOut/LogOut'
+import sprite from '../../../src/images/icons.svg';
 
-const Sidebar = () => {
+export const Sidebar = () => {
+  const dispatch = useDispatch();
+  const [isOpen, setIsOpen] = useState(false);
+  
+
+
   return (
-    <div>
+    <>
         <svg className={s.iconMenu}>
+        <use href={sprite + '#icon-login'}></use>
         </svg>
-         <div className={s.sidebar}>
+         <div className={s.container}>
+         <div className={styles.navigation}>
             <div className={s.title}>
                 <div className={s.logo}>
                 <svg className={s.logoIcon}>
+                <use href={sprite + '#icon-login'}></use>
               </svg>
                 </div>
                 <h2 className={s.mainTitle}>Task Pro</h2>
             </div>
             <CreateNewBoard />
          </div>
+         <nav className={s.dashboards}>
          <MyBoards/>
+         </nav>
          <div className={s.needHelp}>
             <NeedHelp/>
             <LogOut/>
          </div>
-    </div>
+         </div>
+    </>
   )
 }
 
