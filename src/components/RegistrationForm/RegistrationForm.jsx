38537d05ee565a8272693bcdfa4b3de38486ee22
Sergii Minko <minkosergii@gmail.com>
2024-08-06 13:34:42 +0200

    edit design + route /auth/ + sprite eye

--- src/components/RegistrationForm/RegistrationForm.jsx
+++ src/components/RegistrationForm/RegistrationForm.jsx
@@ -5,6 +5,7 @@ import css from './RegistrationForm.module.css';
 // import Loader from '../Loader/Loader';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { NavLink } from 'react-router-dom';
+import icon from '../../images/icons.svg';
 
 const RegistrationForm = () => {
   const isLoading = false; // from redux
@@ -85,27 +86,8 @@ const RegistrationForm = () => {
                 className={css.buttonShowPassword}
                 onClick={togglePasswordVisibility}
               >
-                <svg
-                  width="18"
-                  height="18"
-                  viewBox="0 0 18 18"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M0.75 9C0.75 9 3.75 3 9 3C14.25 3 17.25 9 17.25 9C17.25 9 14.25 15 9 15C3.75 15 0.75 9 0.75 9Z"
-                    stroke="white"
-                    strokeWidth="1.3"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  />
-                  <path
-                    d="M9 11.25C10.2426 11.25 11.25 10.2426 11.25 9C11.25 7.75736 10.2426 6.75 9 6.75C7.75736 6.75 6.75 7.75736 6.75 9C6.75 10.2426 7.75736 11.25 9 11.25Z"
-                    stroke="white"
-                    strokeWidth="1.3"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  />
+                <svg width="20px" height="20px">
+                  <use href={icon + '#icon-eye'}></use>
                 </svg>
               </button>
             </div>
