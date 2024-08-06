a9c56bec4a73d9aeadb7c3600b6a47cc5236288a
Lubov Parfonova <87125430+Lubov1506@users.noreply.github.com>
2024-08-06 17:00:14 +0300

    add theme, userInfo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,10 +6,22 @@ import {
   customStyles,
   selectOptions,
 } from './selectSettings.jsx';
+import { selectUserName } from '../../redux/user/userSelectors.js';
+import { useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
+import { openModal } from '../../redux/modal/modalSlice.js';
+import Modal from '../Modal/Modal.jsx';
 
 const Header = () => {
+  const dispatch = useDispatch();
   const [theme, setTheme] = useState('dark');
-
+  //   const userName = useSelector(selectUserName);
+  const userName = 'Name';
+  //   const avatar = useSelector(selectUserAvatar);
+  const avatar = null;
+  const handleOpenModal = () => {
+    dispatch(openModal());
+  };
   const handleChange = selectedOption => {
     setTheme(selectedOption);
     document.documentElement.setAttribute('theme', selectedOption.value);
@@ -25,12 +37,14 @@ const Header = () => {
         styles={customStyles}
         components={customComponents}
       />
-      <section className={s.user_info}>
-        <p>Name</p>
+      <section className={s.user_info} onClick={handleOpenModal}>
+        <p>{userName}</p>
         <div className={s.img_wrap}>
-          <img src="" alt="" />
+          <img src={avatar} alt="" />
         </div>
       </section>
+
+      <Modal></Modal>
     </header>
   );
 };
