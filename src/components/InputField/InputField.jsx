3273008074307256337b3bd26794d7dd6def6bbf
Lubov Parfonova <87125430+Lubov1506@users.noreply.github.com>
2024-08-07 16:42:32 +0300

    add Input/TextArea component

--- src/components/InputField/InputField.jsx
+++ src/components/InputField/InputField.jsx
@@ -1,8 +1,12 @@
 import clsx from 'clsx';
 import s from './InputField.module.css';
+
 const InputField = ({
   autoComplete = 'off',
   className,
+  isTextArea = false,
+  height,
+  width,
   type = 'text',
   placeholder = 'placeholder',
   register = () => {},
@@ -10,8 +14,24 @@ const InputField = ({
   errors,
   ...rest
 }) => {
+  const style = {
+    ...(height && { height }),
+    ...(width && { width }),
+  };
   if (errors && errors[name]) {
     return <p className={css.errorStyles}>{errors[name].message}</p>;
+  }
+  if (isTextArea) {
+    return (
+      <div className={clsx(s.input_field, s.textarea_container)}>
+        <textarea
+          className={clsx(s.textarea_field)}
+          placeholder={placeholder}
+          style={style}
+          {...rest}
+        />
+      </div>
+    );
   }
   return (
     <input
