da9f58fda690b51b2880ec89e2883d0bb7b3c804
Yelyzaveta31 <Didenkoelizabeth@gmail.com>
2024-08-07 19:26:04 +0300

    Update userOperations.js

--- src/redux/user/userOperations.js
+++ src/redux/user/userOperations.js
@@ -1,5 +1,6 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import { taskProApi } from '../../config/api';
+import axios from 'axios';
 
 export const setToken = accessToken => {
   taskProApi.defaults.headers.common.Authorization = `Bearer ${accessToken}`;
