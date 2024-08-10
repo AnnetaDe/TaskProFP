46db451dc035fed30995defc3740e82754973462
AnnetaDe <anneta.liss@gmail.com>
2024-08-10 21:55:13 +0200

    tasks

---
+++ src/components/Column/Column.jsx
@@ -0,0 +1,3 @@
+export const Column = ({ column }) => {
+  return <li>{column}</li>;
+};
--- src/redux/columns/columnsSelectors.js
+++ src/redux/columns/columnsSelectors.js
@@ -3,3 +3,4 @@ export const selectBoardId = state => state.columns.boardId;
 export const selectBoardTitle = state => state.columns.boardTitle;
 export const selectBoardIcon = state => state.columns.boardIcon;
 export const selectBoardBackground = state => state.columns.boardBackground;
+export const selectTasksWithinColumn = state => state.columns.tasksWithinColumn;
