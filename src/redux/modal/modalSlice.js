41523970551518ebfd5e814446b987a64f4ef716
Lubov Parfonova <87125430+Lubov1506@users.noreply.github.com>
2024-08-11 11:51:45 +0300

    some change

--- src/redux/modal/modalSlice.js
+++ src/redux/modal/modalSlice.js
@@ -4,6 +4,8 @@ const modalSlice = createSlice({
   name: 'modal',
   initialState: {
     isOpen: false,
+    isEditProfile: false,
+    isCreateBoardOpen: false,
   },
   reducers: {
     openModal: state => {
@@ -12,9 +14,28 @@ const modalSlice = createSlice({
     closeModal: state => {
       state.isOpen = false;
     },
+    openEditProfileModal: state => {
+      state.isEditProfile = true;
+    },
+    closeEditProfileModal: state => {
+      state.isEditProfile = false;
+    },
+    openCreateBoardModaal: state => {
+      state.isCreateBoardOpen = true;
+    },
+    closeCreateBoardModaal: state => {
+      state.isCreateBoardOpen = false;
+    },
   },
 });
 
-export const { openModal, closeModal } = modalSlice.actions;
+export const {
+  openModal,
+  closeModal,
+  openEditProfileModal,
+  closeEditProfileModal,
+  openCreateBoardModaal,
+  closeCreateBoardModaal,
+} = modalSlice.actions;
 
 export const modalReducer = modalSlice.reducer;
