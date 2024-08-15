fc5f8f94685be0f72b5710ce181c1c6bdda09d31
tymur-lvov <155083435+tymur-lvov@users.noreply.github.com>
2024-08-15 15:45:50 +0300

    Update Board.jsx

--- src/components/Board/Board.jsx
+++ src/components/Board/Board.jsx
@@ -9,7 +9,7 @@ import {
   selectColumnsOrderId,
   selectColumnsWithinBoard,
   selectFilter,
-  selectFilteredColumns,
+  selectfilteredColumns,
   selectTasksOrderId,
   selectTasksWithinColumn,
 } from '../../redux/columns/columnsSelectors';
