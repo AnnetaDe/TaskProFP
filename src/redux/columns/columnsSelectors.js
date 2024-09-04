7ac83b588b9a80221bced60f42fc6f0be8bbbc19
AnnetaDe <anneta.liss@gmail.com>
2024-09-04 20:56:11 +0200

    1

--- src/redux/columns/columnsSelectors.js
+++ src/redux/columns/columnsSelectors.js
@@ -5,11 +5,9 @@ export const selectBoardTitle = state => state.columns.boardTitle;
 export const selectBoardIcon = state => state.columns.boardIcon;
 export const selectBoardBackground = state => state.columns.boardBackground;
 export const selectTasksWithinColumn = state => state.columns.tasksWithinBoard;
-// export const selectFilteredColumns = state => state.columns.filteredColumns;
-// export const selectFilter = state => state.columns.filter;
+
 export const selectColumnsWithinBoard = state => state.columns.columnsL;
-// export const selectColumnsOrderId = state => state.columns.columnsOrderId;
-// export const selectTasksOrderId = state => state.columns.tasksOrderId;
+
 export const selectCurrentBoardId = state => state.columns.currentBoardId;
 export const selectLoadingData = state => state.columns.isLoading;
 export const selectTasks = state => state.columns.tasks;
