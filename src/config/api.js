379d2c1fe40f54b897f29c7a243545400b666dd9
AnnetaDe <anneta.liss@gmail.com>
2025-06-04 11:38:14 +0200

    url

--- src/config/api.js
+++ src/config/api.js
@@ -8,14 +8,14 @@ import axios from 'axios';
 // });
 
 export const taskProApi = axios.create({
-  baseURL: 'http://192.168.1.73:3000/',
+  baseURL: 'https://taskproback-production.up.railway.app/',
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
   },
 });
 export const taskProApiFormData = axios.create({
-  baseURL: 'http://192.168.1.73:3000/',
+  baseURL: 'https://taskproback-production.up.railway.app/',
   withCredentials: true,
   headers: {
     'Content-Type': 'multipart/form-data',
