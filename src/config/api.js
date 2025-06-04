6fe97aaf9094d006519a0ebb006a80f9de9bd939
AnnetaDe <anneta.liss@gmail.com>
2025-06-04 20:56:43 +0200

    upd2

--- src/config/api.js
+++ src/config/api.js
@@ -1,35 +1,35 @@
 import axios from 'axios';
 
 
-// export const taskProApi = axios.create({
-//   baseURL: 'https://taskproback-production.up.railway.app/',
-//   withCredentials: true,
-//   headers: {
-//     'Content-Type': 'application/json',
-//   },
-// });
-
-// export const taskProApiFormData = axios.create({
-//   baseURL: 'https://taskproback-production.up.railway.app/',
-//   withCredentials: true,
-//   headers: {
-//     'Content-Type': 'multipart/form-data',
-//   },
-// });
-
 export const taskProApi = axios.create({
-  baseURL: 'http://192.168.1.73:3000/',
+  baseURL: 'https://taskproback-production.up.railway.app/',
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
   },
 });
 
-
 export const taskProApiFormData = axios.create({
-  baseURL: 'http://192.168.1.73:3000/',
+  baseURL: 'https://taskproback-production.up.railway.app/',
   withCredentials: true,
   headers: {
     'Content-Type': 'multipart/form-data',
   },
 });
+
+// export const taskProApi = axios.create({
+//   baseURL: 'http://192.168.1.73:3000/',
+//   withCredentials: true,
+//   headers: {
+//     'Content-Type': 'application/json',
+//   },
+// });
+// 
+// 
+// export const taskProApiFormData = axios.create({
+//   baseURL: 'http://192.168.1.73:3000/',
+//   withCredentials: true,
+//   headers: {
+//     'Content-Type': 'multipart/form-data',
+//   },
+// });
