7eabfb31b29cc6ddab69c916d72908c545ff719e
AnnetaDe <anneta.liss@gmail.com>
2025-06-07 15:21:31 +0200

    i

--- src/config/interceptors.js
+++ src/config/interceptors.js
@@ -1,6 +1,6 @@
 import { store } from '../redux/store';
 import { logoutThunk, refreshTokensThunk } from '../redux/user/userOperations';
-import { taskProApi } from './api';
+import { taskProApi, taskProApiFormData } from './api';
 
 taskProApi.interceptors.response.use(
   response => response,
@@ -24,3 +24,53 @@ taskProApi.interceptors.response.use(
     return Promise.reject(error);
   }
 );
+
+
+taskProApi.interceptors.response.use(
+  response => response,
+  async error => {
+    const originalRequest = error.config;
+
+    const { status } = error.response || {};
+    if (status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+      try {
+        await store.dispatch(refreshTokensThunk());
+        originalRequest.withCredentials = true;
+        return taskProApi(originalRequest);
+      } catch (refreshError) {
+        store.dispatch(logoutThunk());
+        window.location.href = '/auth/login';
+        return Promise.reject(refreshError);
+      }
+    }
+
+    return Promise.reject(error);
+  }
+);
+
+
+taskProApiFormData.interceptors.response.use(
+  response => response,
+  async error => {
+    const originalRequest = error.config;
+
+    const { status } = error.response || {};
+    if (status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+      try {
+        await store.dispatch(refreshTokensThunk());
+        originalRequest.withCredentials = true;
+        return taskProApi(originalRequest);
+      } catch (refreshError) {
+        store.dispatch(logoutThunk());
+        window.location.href = '/auth/login';
+        return Promise.reject(refreshError);
+      }
+    }
+
+    return Promise.reject(error);
+  }
+);
+
+
