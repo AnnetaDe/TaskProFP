ee7aaf44c3deba72ca59421f9169db80cbeec642
AnnetaDe <anneta.liss@gmail.com>
2025-06-07 10:09:31 +0200

    f

--- src/config/api.js
+++ src/config/api.js
@@ -17,19 +17,4 @@ export const taskProApiFormData = axios.create({
   },
 });
 
-// export const taskProApi = axios.create({
-//   baseURL: 'http://192.168.1.73:3000/',
-//   withCredentials: true,
-//   headers: {
-//     'Content-Type': 'application/json',
-//   },
-// });
-// 
-// 
-// export const taskProApiFormData = axios.create({
-//   baseURL: 'http://192.168.1.73:3000/',
-//   withCredentials: true,
-//   headers: {
-//     'Content-Type': 'multipart/form-data',
-//   },
-// });
+
